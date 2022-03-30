fe4128ff3e9e81246c312aa7c6f501cdf3111262
Alexxix <90107454+Alexxix@users.noreply.github.com>
2022-03-30 15:14:02 -0500

    Solucion1

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sum = 0;
 
+  for (let i = 0; i < num.length; i++) {
+     sum += /*Math.pow(2, num.length-(i+1))* num[1]*/ +num[i] * 2 ** (num.length - 1 - i);
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+// tu codigo aca
 
+/*binary="" num=30     operacion= */
+var binary =""
+while (num>0){
+binary = num%2 + binary
+num = Math.floor(num/2)
+}
+return binary
 }
 
 
